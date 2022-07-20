baecaca3e90d667afc9f9fe2a9cb0d5ab73c1969
Randy Huang <70608993+balsaferret@users.noreply.github.com>
2022-07-19 22:37:40 -0400

    implemented basic backend w/o course controller

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+// const loginRouter = require('./controllers/login')
+const courseRouter = require('./controllers/courses')
+// const usersRouter = require('./controllers/users')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI
+
+// mongoose.connect(mongoUrl)
+
+app.use(cors())
+// app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+// app.use(middleware.tokenExtractor)
+
+// app.use('/api/login', loginRouter)
+app.use('/api/courses', courseRouter)
+// app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
